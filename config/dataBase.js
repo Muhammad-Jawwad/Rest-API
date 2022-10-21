e0d063de2641d2620f2f508d371a251fef0f0506
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-10-21 13:34:44 -0700

    Update dataBase.js

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,12 +1,12 @@
-const {createpool} = require("mysql");
+const { createPool } = require("mysql");
 
-const pool = createpool({
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
-    connectionLimit: process.env.LIMIT_DB
+const pool = createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
+  connectionLimit: 10
 });
 
 module.exports = pool;
