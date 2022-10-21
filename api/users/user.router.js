7c42309f96659eeb39486efd6e47c46b0e9e2570
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-10-21 13:59:11 -0700

    Update user.router.js

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-// const { checkToken } = require("../../auth/token_validation");
+const { checkToken } = require("../../auth/token_validation");
+
 const {
   createUser,
   getUserByUserId,
@@ -9,17 +10,13 @@ const {
   login
 } = require("./user.controller");
 
-router.post("/",createUser);
-router.get("/",getUsers);
-router.get("/:id",getUserByUserId);
-router.patch("/",updateUsers);
-router.delete("/",deleteUser);
-router.post("/login",login);
-// router.get("/", checkToken, getUsers);
-// router.post("/", checkToken, createUser);
-// router.get("/:id", checkToken, getUserByUserId);
-// router.post("/login", login);
-// router.patch("/", checkToken, updateUsers);
-// router.delete("/", checkToken, deleteUser);
+
+router.get("/", checkToken, getUsers);
+router.post("/", checkToken, createUser);
+router.get("/:id", checkToken, getUserByUserId);
+router.post("/login", login);
+router.patch("/", checkToken, updateUsers);
+router.delete("/", checkToken, deleteUser);
+
 
 module.exports = router;
