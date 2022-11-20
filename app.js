9c0587af92018d67b6b80f68086ae507b9835b10
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-11-20 14:43:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
-const userRouter = require("./api/users/user.router");
+const bodyParser = require('body-parser');
+const upload = require('express-fileupload');
+const nodemailer = require('nodemailer');
 
-var cors = require('cors')
-app.use(cors())
+
+const userRouter = require("./api/users/user.router");
+const visitorRouter = require("./api/visitors/visitors.router");
+
+const app = express();
+app.use(upload());
+
+
+// var cors = require('cors')
+// app.use(cors())
 
 app.use(express.json());
+app.use(bodyParser.json());
+
 
 app.use("/api/users", userRouter);
+app.use("/api/visitors", visitorRouter);
+
 
 app.listen(process.env.APP_PORT, () => {
   console.log("server up and running on PORT :", process.env.APP_PORT);
